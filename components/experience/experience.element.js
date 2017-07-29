0d5a079717cd5b249752999fff52a61828c3f9b7
iain plimmer <iainplimmer@iains-iMac.local>
2017-07-29 09:45:50 +0100

    Removal of un-needed component and copy additions.

--- components/experience/experience.element.js
+++
@@ -1,8 +0,0 @@
-(function (customElements) {
-	class ExperienceElement extends Polymer.Element {                        
-		static get is() { 
-			return 'experience-element'; 
-		}
-	}
-	customElements.define(ExperienceElement.is, ExperienceElement);
-})(customElements);        
